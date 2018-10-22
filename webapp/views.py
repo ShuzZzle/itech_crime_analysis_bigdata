1d37f3699c70afc86df758d1e05cf040ce1b8760
Merlin Reichwald <merlin.reichwald@gmail.com>
2018-10-22 11:33:45 +0200

    os separator path

--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
+from pathlib import Path
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'dest\index.html')
-
+    return render(request, Path('dest/index.html'))
